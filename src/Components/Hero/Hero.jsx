8333c1c88ea589c8a30734155626bd7244396d39
muhammadshehroz897@gmail.com <muhammadshehroz897@gmail.com>
2024-08-21 03:25:58 -0700

    remove button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -64,32 +64,6 @@ const Hero = () => {
             </div>
           </div>
         </div>
-
-        {/* Carousel Controls */}
-        {/* <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button> */}
       </div>
 
       {/* Menu Section */}
